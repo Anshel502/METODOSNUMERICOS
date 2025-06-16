c3a5065a6e86e58eb9c4c07ac3ded85b7fb74c6c
Yael Lopez <l.locj050605@itses.edu.mx>
2025-06-16 09:18:04 -0600

    Primer commit

---
+++ MetodosNumericos/src/main/java/mx/edu/itses/jylc/MetodosNumericos/services/UnidadIIService.java
@@ -0,0 +1,9 @@
+package mx.edu.itses.jylc.MetodosNumericos.services;
+
+import java.util.ArrayList;
+import mx.edu.itses.jylc.MetodosNumericos.domain.Biseccion;
+
+public interface UnidadIIService {
+     public ArrayList<Biseccion> AlgoritmoBiseccion(Biseccion biseccion);
+     
+}
